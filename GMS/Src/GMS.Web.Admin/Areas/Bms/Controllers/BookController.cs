52b86121846842dd887754fdd3cdf6de01e1b3fc
lakeoffaith <905663280@qq.com>
2016-11-13 17:33:51 +0800

    Signed-off-by: lakeoffaith <905663280@qq.com>

---
+++ GMS/Src/GMS.Web.Admin/Areas/Bms/Controllers/BookController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GMS.Web.Admin.Areas.Bms.Controllers
+{
+    public class BookController : Controller
+    {
+        //
+        // GET: /Bms/Book/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
